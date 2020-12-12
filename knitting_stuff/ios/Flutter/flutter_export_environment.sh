c8a58273caace6ebb9498d98ec29db5850625751
MooMonk <jiahuii.ho@gmail.com>
2020-12-12 13:30:50 +0800

    update user.dart

--- knitting_stuff/ios/Flutter/flutter_export_environment.sh
+++ knitting_stuff/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Git\FlutterA201\food_ninja"
+export "FLUTTER_APPLICATION_PATH=C:\Git\KnittingStuff\knitting_stuff"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- knitting_stuff/lib/loginscreen.dart
+++ knitting_stuff/lib/loginscreen.dart
@@ -6,6 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toast/toast.dart';
 import 'package:http/http.dart' as http;
 import 'package:knitting_stuff/mainscreen.dart';
+import 'package:knitting_stuff/user.dart';
 
 
 class LoginScreen extends StatefulWidget {
